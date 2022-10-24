b5025302cbcab9426a5c61e4af4c694316b731af
marcoaufiero <marcoaufiero96@gmail.com>
2022-10-24 15:16:57 +0200

    js var added (to be fixed)

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let mailList = ['giorgio99@gmail.com','fabio77@gmail.com','davide44@gmail.com'];
+
+console.log(mailList)
+
+function submit(){
+    
+    let mailName = document.getElementById('inputMail').value;
+    console.log(mailName);
+
+    
+
+}
